ea93cd005df82b35dc0fc56518047fb70b5028bf
Mitrokhin Denis <mitrde@mail.ru>
2020-04-19 14:32:43 +0300

    addded 7th task

--- cpp1_task_07_threads/src/main.cpp
+++ cpp1_task_07_threads/src/main.cpp
@@ -2,9 +2,99 @@
 #include <list>
 #include<vector>
 #include<future>
+#include <condition_variable>
+#include <functional>
+#include <thread>
+#include <queue>
 
 using namespace std;
 
+
+class ThreadPool
+{
+public:
+	using Task = std::function<void()>;
+
+	explicit ThreadPool(std::size_t numThreads)
+	{
+		start(numThreads);
+	}
+
+	~ThreadPool()
+	{
+		stop();
+	}
+
+	template<class T>
+	auto enqueue(T task)->std::future<decltype(task())>
+	{
+		auto wrapper = std::make_shared<std::packaged_task<decltype(task()) ()>>(std::move(task));
+
+		{
+			std::unique_lock<std::mutex> lock{ mEventMutex };
+			cout << "task added" << endl;
+			mTasks.emplace([=] {
+				(*wrapper)();
+			});
+		}
+
+		mEventVar.notify_one();
+		return wrapper->get_future();
+	}
+
+private:
+	std::vector<std::thread> mThreads;
+
+	std::condition_variable mEventVar;
+
+	std::mutex mEventMutex;
+	bool mStopping = false;
+
+	std::queue<Task> mTasks;
+
+	void start(std::size_t numThreads)
+	{
+		for (auto i = 0u; i < numThreads; ++i)
+		{
+			mThreads.emplace_back([=] {
+				while (true)
+				{
+					Task task;
+
+					{
+						std::unique_lock<std::mutex> lock{ mEventMutex };
+
+						mEventVar.wait(lock, [=] { return mStopping || !mTasks.empty(); });
+
+						if (mStopping && mTasks.empty())
+							break;
+
+						task = std::move(mTasks.front());
+						mTasks.pop();
+						//cout << "task poped" << endl;
+					}
+
+					task();
+					
+				}
+			});
+		}
+	}
+
+	void stop() noexcept
+	{
+		{
+			std::unique_lock<std::mutex> lock{ mEventMutex };
+			mStopping = true;
+		}
+
+		mEventVar.notify_all();
+
+		for (auto &thread : mThreads)
+			thread.join();
+	}
+};
+//
 template <typename T>
 list<T> merge(list<T> && first, list<T> &&second)
 {
@@ -22,66 +112,117 @@ list<T> merge(list<T> && first, list<T> &&second)
 			second.pop_front();
 		}
 	}
-	if (!second.empty())
+	while (!second.empty())
 	{
-		while (!second.empty())
-		{
-			tmp.push_back(second.front());
-			second.pop_front();
-		}
+		tmp.push_back(second.front());
+		second.pop_front();
 	}
-	else
+	while (!first.empty())
 	{
 		tmp.push_back(first.front());
 		first.pop_front();
 	}
+	for (auto it : tmp)
+		cout << it << " ";
+	cout << endl;
 	return tmp;
 }
 
+//
+
 int main()
 {
+	std::mutex glb_mtx;
 	int threads = 0;
+	using type = int;
+
+
 	cout << "input amount of threads" << endl;
 	cin >> threads;
-	vector<future<list<int>>> results(threads);
+	ThreadPool pool(threads);
+
+	
+	
 
 	int num = 0;
 	cout << "input amount of numbers" << endl;
 	cin >> num;
 
-	int tmp = 0;
-
-	list<list<int>> tasks(num);
-
-	for (auto &it : tasks)
+	int val = (num+1)/2;
+	int iterations = 0;
+	
+	while (val != 1)
 	{
-		cin >> tmp;
-		it.push_back(tmp);
+		int tmp;		
+		tmp = val % 2;
+		val >>= 1;
+		iterations += val;
+		val += tmp;
+	}
+	
+	list<list<int>> items(num);
+	list<future<list<int>>> futures;
+
+	for (auto &it: items)
+	{
+		cin >> val;
+		it.push_back(val);
 	}
 
-	while (tasks.size() > 1)
-	{
-		int i = 0;
-
-		for (; tasks.size() > 1 && i < threads; ++i)
-		{
-			auto tmp1 = tasks.front();
-			tasks.pop_front();
-			auto tmp2 = tasks.front();
-			tasks.pop_front();
-			results[i] = (async(std::launch::async, merge<int>, move(tmp1), move(tmp2)));
-		}
-		
-		for (int j = 0; j < i; ++j)
-			tasks.emplace_back(results[j].get());
-		
+	for (int i = 0; i<(num+1)/2;++i)
+	{		
+		futures.emplace_back(pool.enqueue([&items,&glb_mtx]() {//HOW TO STORE A FUNCTION WITH A VALUE?
+			list<type> first = {};
+			list<type> second = {};
+			{
+				unique_lock<mutex> lock{ glb_mtx };
+				first = items.front();
+				items.pop_front();
+				if (!items.empty())
+				{
+					second = items.front();
+					items.pop_front();
+				}
+				//cout << "items poped<<<<<<<<<<<<<<<<< "/*<<items.size()*/ << endl;
+			}			
+			return merge(move(first),move(second));
+		}));		
 	}
+	//cout <<"number of futures "<< futures.size() << endl;
 
-	for (auto it : tasks.front())
+	for (int i = 0; i < iterations; ++i)
+	{
+		futures.emplace_back(pool.enqueue([&futures, &glb_mtx]() {
+			list<type> first = {};
+			list<type> second = {};
+			{
+				unique_lock<mutex> lock{ glb_mtx };
+				first = futures.front().get();
+				futures.pop_front();
+				if (!futures.empty())
+				{
+					second = futures.front().get();
+					futures.pop_front();
+				}
+				//cout << "futures poped<<<<<<<<<<<<<<" /*<< futures.size()*/ << endl;
+			}
+			
+			return merge(move(first), move(second));
+		}));
+	}
+	//cout << "number of futures " << futures.size() << endl;
+	while (futures.size() >1)
+	{
+	
+	}
+	//cout << "number of futures " << futures.size() << endl;
+
+	list<type> promises = futures.front().get();
+	for (auto it : promises)
 	{
 		cout << it << " ";
 	}
-	cout << endl;
+	cout << promises.size() << endl;
 
 
 
